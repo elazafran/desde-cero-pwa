0adaff1e75428c692475f8e478f1a22975367d98
Javier Aliaga <javiealiaga@gmail.com>
2018-05-08 09:57:22 +0200

    service Workers cache funcionando

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
+const staticAssets = [
+    './',
+    './styles.css',
+    './app.js'
 
+];
 
 //el primer punto el install
-self.addEventListener('install', event =>{
-    console.log(`install`);
-    
+self.addEventListener('install', async event =>{
+    // aqui se cachean pero aún no están disponibles
+    const cache = await caches.open('news-static');
+    cache.addAll(staticAssets);
 });
 
 //
 self.addEventListener('fetch',event =>{
-    console.log(`fetch`);
+    //console.log(`fetch`);
+    const req = event.request;
+    event.respondWith(cacheFirst(req))
     
 });
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req)
+    return cachedResponse || fetch(req);
+}
