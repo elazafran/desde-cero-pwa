2691cf431608378ecfe04dc04761b40430cca6b9
Javier Aliaga <javiealiaga@gmail.com>
2018-05-08 09:09:10 +0200

    fetch contenido

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-//cc7baf6c7a0b44d4814e926312bc07df
+'use strict';
+//https://newsapi.org/v2/sources?apiKey=cc7baf6c7a0b44d4814e926312bc07df
 //https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc7baf6c7a0b44d4814e926312bc07df
 const apikey = "cc7baf6c7a0b44d4814e926312bc07df"
 const main = document.querySelector('main');
+const sourceSelect = document.querySelector('#sourceSelect');
 window.addEventListener('load', e => {
     updateNews();
+    updateSources();
 });
-
+async function updateSources(){
+    
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=cc7baf6c7a0b44d4814e926312bc07df`);
+    const json  = await res.json();
+    sourceSelect.innerHTML =  json.sources.map(source => `<option value="${source.id}">${source.name}</option>`).join('\n');
+}
 async function updateNews() {
     const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc7baf6c7a0b44d4814e926312bc07df');
     const json = await res.json();
@@ -22,5 +30,4 @@ function createArticle(article) {
     </a>
   </div>
     `;
-}//cc7baf6c7a0b44d4814e926312bc07df
-//https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc7baf6c7a0b44d4814e926312bc07df
+}
