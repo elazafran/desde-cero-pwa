fb36d761521a3a61314477e6598009eeb92fee00
Javier Aliaga <javiealiaga@gmail.com>
2018-05-08 09:17:19 +0200

    update select contenido

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict';
 //https://newsapi.org/v2/sources?apiKey=cc7baf6c7a0b44d4814e926312bc07df
 //https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc7baf6c7a0b44d4814e926312bc07df
-const apikey = "cc7baf6c7a0b44d4814e926312bc07df"
+const apiKey = 'cc7baf6c7a0b44d4814e926312bc07df';
 const main = document.querySelector('main');
 const sourceSelect = document.querySelector('#sourceSelect');
-window.addEventListener('load', e => {
+const defaultSource = 'the-washington-post';
+
+window.addEventListener('load', async e => {
     updateNews();
-    updateSources();
+    await updateSources();
+    sourceSelect.value = defaultSource;
+    //cuando cambie el valor lanzamos el update de la función
+    sourceSelect.addEventListener('change',e =>{
+        updateNews(e.target.value);
+    });
 });
 async function updateSources(){
     
@@ -14,8 +21,8 @@ async function updateSources(){
     const json  = await res.json();
     sourceSelect.innerHTML =  json.sources.map(source => `<option value="${source.id}">${source.name}</option>`).join('\n');
 }
-async function updateNews() {
-    const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc7baf6c7a0b44d4814e926312bc07df');
+async function updateNews(source = defaultSource) {
+    const res =  await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&sortBy=top&apiKey=${apiKey}`);
     const json = await res.json();
     main.innerHTML = json.articles.map(createArticle).join('\n');
 }
