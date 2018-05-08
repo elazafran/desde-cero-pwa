0c7d198564c07baaedd1c38a56eb4bd629d1aabe
Javier Aliaga <javiealiaga@gmail.com>
2018-05-08 10:37:46 +0200

    opcion por defecto si no hay contenido

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 const staticAssets = [
     './',
     './styles.css',
-    './app.js'
+    './app.js',
+    './fallback.json',
+    './images/fetch-dog.jpg',
 
 ];
 
@@ -16,10 +18,28 @@ self.addEventListener('install', async event =>{
 self.addEventListener('fetch',event =>{
     //console.log(`fetch`);
     const req = event.request;
-    event.respondWith(cacheFirst(req))
+    const url = new URL(req.url);
+    if(url.origin === location.origin){
+        event.respondWith(cacheFirst(req));
+    }else {
+        event.respondWith(networkFirst(req));
+    }
     
 });
+
 async function cacheFirst(req) {
     const cachedResponse = await caches.match(req)
     return cachedResponse || fetch(req);
+}
+async function networkFirst(req){
+    const cache = await caches.open('news-dynamic');
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedResponse = await cache.match(req);
+        return cachedResponse || caches.match('./fallback.json');
+    }
+
 }
